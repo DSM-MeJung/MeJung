7d1f4109d7f9935a5f5798e9a8630b1f947c0117
Pink_Poma <choi82354@gmail.com>
2023-05-24 20:21:47 +0900

    aa

--- Assets/Script/MonsterSpawn.cs
+++ Assets/Script/MonsterSpawn.cs
@@ -16,7 +16,7 @@ public class MonsterSpawn : MonoBehaviour
     private IEnumerator Spawn()
     {
         yield return new WaitForSeconds(Random.Range(8f, 20f));
-        if (Random.Range(0f, 10f) > 9)
+        if (Random.Range(0f, 10f) > 9.5f)
         {
             Instantiate(monster[Random.Range(0, monster.Length)],
                 new Vector3(transform.position.x, transform.position.y + 2), Quaternion.identity); 
